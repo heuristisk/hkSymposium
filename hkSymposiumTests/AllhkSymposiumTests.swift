1bac1e991a472be59afae1ba69571297108def2c
Anderson Gusmão <anderson.santos.gusmao@gmail.com>
2017-12-29 20:45:58 -0200

    setting up a new project and adjusting swift lint.

---
+++ hkSymposiumTests/AllhkSymposiumTests.swift
@@ -0,0 +1,29 @@
+//
+//  hkSymposiumTests.swift
+//  hkSymposiumTests
+//
+//  Created by Anderson Santos Gusmão on 29/12/2017.
+//  Copyright © 2017 Anderson Santos Gusmão. All rights reserved.
+//
+
+import XCTest
+@testable import hkSymposium
+
+class AllhkSymposiumTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testExample() {
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+}
