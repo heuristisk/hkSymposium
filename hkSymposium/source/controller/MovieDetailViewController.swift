f3bf556c617a3950f8a86c442fb5bdbd503801cb
Anderson Gusmão <anderson.santos.gusmao@gmail.com>
2017-12-31 17:37:47 -0200

    Adding detail view controller

--- hkSymposium/source/controller/MovieDetailViewController.swift
+++ hkSymposium/source/controller/MovieDetailViewController.swift
@@ -11,6 +11,30 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
 
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var voteAverageProgressBar: UIProgressView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelReleaseDate: UILabel!
+    @IBOutlet weak var labelOverview: UILabel!
     var movie: Movie!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+
+    }
+
+    private func setupUI() {
+        if movie.urlImage != String.Empty {
+            self.posterImageView.kf.indicatorType = .activity
+            self.posterImageView.kf.setImage(with: URL(string: movie.urlImage))
+        } else {
+            self.posterImageView.image = UIImage(named: "ImageNotFound")
+        }
+
+        self.voteAverageProgressBar.setProgress(Float(self.movie.voteAverage / 10), animated: true)
+        self.labelOverview.text = movie.overview
+        self.labelTitle.text = movie.title
+        self.labelReleaseDate.text = (movie.releaseDate?.toString()) ?? String.Empty
+    }
 }
